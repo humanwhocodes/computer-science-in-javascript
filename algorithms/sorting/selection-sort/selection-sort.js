543ee9fab979c4e2c55df68173823aeedc0f2d8d
Nicholas C. Zakas <nicholas.c.zakas@nczonline.net>
2009-09-07 11:17:41 -0700

    Selection sort implementation

---
+++ algorithms/sorting/selection-sort/selection-sort.js
@@ -0,0 +1,68 @@
+/*
+ * Selection sort implementation in JavaScript
+ * Copyright (c) 2009 Nicholas C. Zakas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+
+/**
+ * Swaps two values in an array.
+ * @param {Array} items The array containing the items.
+ * @param {int} firstIndex Index of first item to swap.
+ * @param {int} secondIndex Index of second item to swap.
+ * @return {void}
+ */
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+ 
+/**
+ * A selection sort implementation in JavaScript. The array
+ * is sorted in-place.
+ * @param {Array} items An array of items to sort.
+ * @return {Array} The sorted array.
+ */
+function selectionSort(items){
+
+    var len = items.length,
+        min;
+
+    for (i=0; i < len; i++){
+    
+        //set minimum to this position
+        min = i;
+        
+        //check the rest of the array to see if anything is smaller
+        for (j=i+1; j < len; j++){
+            if (items[j] < items[min]){
+                min = j;
+            }
+        }
+        
+        //if the minimum isn't in the position, swap it
+        if (i != min){
+            swap(items, i, min);
+        }
+    }
+    
+    return items;
+}
