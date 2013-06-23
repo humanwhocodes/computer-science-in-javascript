358ff658185ebea8315e296c74e7e9c936b44ab9
George Kats <katsgeorgeek@gmail.com>
2013-06-23 12:42:35 +0300

    Add shell sort algorithm.

---
+++ algorithms/sorting/shell-sort/shell-sort.js
@@ -0,0 +1,38 @@
+/**
+ * Swaps two values in an array.
+ * @param {Array} items The array containing the items.
+ * @param {int} firstIndex Index of first item to swap.
+ * @param {int} secondIndex Index of second item to swap.
+ * @return {void}
+ */
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+/**
+ * A shell sort implementation in JavaScript.
+ * @param {Array} items An array of items to sort.
+ * @return {Array} The sorted array.
+ */
+function shellSort(items) {
+	var	step = items.length,
+		i, j;
+
+	do {
+		step = Math.floor(step/2);
+		i = 0;
+		j = i + step;
+		while (j < arr.length) {
+			if (items[i] > items[j]) {
+				swap(items, i, j);
+			}
+
+			i++;
+			j = i + step;
+		}
+	} while (step >= 1);
+
+	return items;
+}
