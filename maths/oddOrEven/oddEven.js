a7cd6eb4f0c61afdc36a51485cee8004f3e43320
zsmahi <zakaria08esi@gmail.com>
2013-08-29 01:44:29 +0100

    new file:   oddEven.js

---
+++ maths/oddOrEven/oddEven.js
@@ -0,0 +1,13 @@
+/* Created by SMAHI Zakaria <zakaria08esi@gmail.com>
+
+in August 29th, 2013 at 00:30 GMT
+test is a number is odd or even 
+
+*/
+
+
+function even(number){
+number=parseInt(number);
+return ((number &amp; 1)=='0')?true:false;
+}
+
