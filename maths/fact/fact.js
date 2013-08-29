f8f894945182a3c5f4f11c41da9b5df503326a43
zsmahi <zakaria08esi@gmail.com>
2013-08-29 02:50:39 +0100

    new file:   ../fact/fact.js 	new file:   ../fact/factorial.txt 	new file:   oddEven.txt

---
+++ maths/fact/fact.js
@@ -0,0 +1,11 @@
+/* factorial(n)
+by Zakaria SMAHI <zakaria08esi@gmail.com>
+
+created on august 29th,2013 at 01:44 gmt 
+
+*/
+
+
+ function Factorial(n) {
+       return n > 1?n * Factorial(n-1):1;
+    }
